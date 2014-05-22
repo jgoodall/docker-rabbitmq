8ddaba08865856fadf53e718a526e48e26b6f5ad
John Goodall <jgoodall@gmail.com>
2014-05-22 12:26:15 -0400

    password setting wasnt being run correctly, moved it to run script

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+if [ ! -f /.rabbitmq_password_set ]; then
+  /set_rabbitmq_password.sh
+fi
+
 exec supervisord -n
--- start_rabbitmq.sh
+++ start_rabbitmq.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-
-if [ ! -f /.rabbitmq_password_set ]; then
-  /set_rabbitmq_password.sh
-fi
-
 exec /usr/sbin/rabbitmq-server
